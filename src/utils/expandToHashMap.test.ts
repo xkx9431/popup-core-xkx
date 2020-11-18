e04d81ff6d8b7ada4a455df624bdda154035fa0b
Kevin, Xu <kaixuan.xu@siemens.com>
2020-11-18 15:45:37 +0800

    add test the expandHashtype side test

---
+++ src/utils/expandToHashMap.test.ts
@@ -0,0 +1,7 @@
+import expandToHashMap from './expandToHashMap';
+
+it('expands all the possible cases', () => {
+  const listeners = ['scroll', 'resize'];
+  expect(expandToHashMap(true, listeners)).toMatchSnapshot('true');
+  expect(expandToHashMap(false, listeners)).toMatchSnapshot('false');
+});
