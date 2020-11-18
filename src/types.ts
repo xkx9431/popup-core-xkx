17b96b67c6d6cf8c6a2318a51a6b9d9e2d119e97
Kevin, Xu <kaixuan.xu@siemens.com>
2020-11-18 14:27:00 +0800

    add debounce util

---
+++ src/types.ts
@@ -0,0 +1,19 @@
+
+
+export type Rect = {
+    width:number,
+    height:number,
+    x:number,
+    y:number
+}
+
+export type ClientRectObject = {
+    x: number,
+    y: number,
+    top: number,
+    left: number,
+    right: number,
+    bottom: number,
+    width: number,
+    height: number,
+}
