b6e837bc2be73a7d4010d1ddc0bca54a29bb63a9
Kevin, Xu <kaixuan.xu@siemens.com>
2020-11-18 11:21:08 +0800

    config build es, commoon js

--- src/utils/uniqueBy.ts
+++ src/utils/uniqueBy.ts
@@ -1,5 +1,5 @@
 
-
+// get unique identifier by assigned key
 export default function uniqueBy<T> (arr: Array<T>, fn: (name:T) => any ): Array<T> {
     const identifiers = new Set();
     return arr.filter( item =>{
